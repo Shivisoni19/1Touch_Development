8a24e7c872f00170a9cc392c8874e1cc18b9cd16
shivani soni <kajalsoni1845@gmail.com>
2022-06-14 18:56:57 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import './utils/Color.css'
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import CustomNavbar from './components/CustomNavbar'
+import Footer from './components/Footer'
+import Home from "./Home"
+import About from "./About"
+import Contact from "./Contact"
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+// import ProgressBar from "./components/ProgressBar";
 
-function App() {
+
+function App() { 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <h1>hello</h1>
+    <div className="bg-color">
+      <Router>
+        <Header/>
+        <CustomNavbar/>
+        {/* <ProgressBar/> */}
+          <div className="fluid-container main-page">
+          <Routes>
+            <Route exact path="/" element={<Home/>} />
+            <Route exact path="about" element={<About/>} />
+            <Route exact path="contact" element={<Contact/>} />
+            <Route exact path="/slider" element={<Slider/>} />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
